375c79db733b27d5acb4669d4b9783ea63d4b287
Mark Bieganek <markbieganek@gmail.com>
2022-10-03 10:03:01 -0600

    Add basic click event functionality

--- javascript/introduction-to-js-1/assessment-start/script.js
+++ javascript/introduction-to-js-1/assessment-start/script.js
@@ -1,14 +1,29 @@
+function onClick() {
+  // Validate name input
+  if (inputIsEmpty(customName)) {
+    // No name was entered, abort and print error.
+    console.log("error: name is empty");
+    return;
+  } else {
+
+  }
+}
+
 function inputIsEmpty(input) {
-  input !== '' ? true : false;
+  if (input === '') {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function randomValueFromArray(array){
-  const random = Math.floor(Math.random()*array.length);
-  return array[random];
+const random = Math.floor(Math.random()*array.length);
+return array[random];
 }
 
-const customName = document.getElementById('customname');
+const customName = document.getElementById('customname').textContent;
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-const inputIsValid = inputIsEmpty(customName);
+randomize.addEventListener('click', (event) => {onClick()});
